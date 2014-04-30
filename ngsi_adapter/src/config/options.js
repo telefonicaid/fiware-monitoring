207e1f9c553d71dd797a706af7ad3a7a32a7d0d3
Pablo Rodriguez <pra@tid.es>
2014-04-30 17:15:00 +0200

    Add JSDoc documentation to Node.js sources

--- ngsi_adapter/src/config/options.js
+++ ngsi_adapter/src/config/options.js
@@ -1,25 +1,39 @@
-//
-// Copyright 2013 Telefónica I+D
-// All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License"); you may
-// not use this file except in compliance with the License. You may obtain
-// a copy of the License at
-//
-//       http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations
-// under the License.
-//
+/*
+ * Copyright 2013 Telefónica I+D
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+/**
+ * Module that defines server configuration options.
+ *
+ * @module options
+ */
 
 
 'use strict';
 
 
-// Default values for command line options
+/**
+ * @namespace
+ * @property {Object} defaults              Default values for configuration options.
+ * @property {String} defaults.brokerUrl    Default Context Broker URL.
+ * @property {String} defaults.listenHost   Default adapter listen host.
+ * @property {Number} defaults.listenPort   Default adapter listen port.
+ * @property {Number} defaults.retries      Default maximum number of invocation retries.
+ */
 var defaults = {
     brokerUrl:  'http://127.0.0.1:1338/',
     listenHost: '127.0.0.1',
@@ -28,7 +42,14 @@ var defaults = {
 };
 
 
-// Parse command line options
+/**
+ * @namespace
+ * @property {Object} opts                  Command line options.
+ * @property {String} opts.brokerUrl        Context Broker URL.
+ * @property {String} opts.listenHost       Adapter listen host.
+ * @property {Number} opts.listenPort       Adapter listen port.
+ * @property {Number} opts.retries          Maximum number of invocation retries.
+ */
 var opts = require('optimist')
     .options('b', { alias: 'brokerUrl',  'default': defaults.brokerUrl,  describe: 'Context Broker URL'  })
     .options('H', { alias: 'listenHost', 'default': defaults.listenHost, describe: 'Adapter listen host' })
--- ngsi_adapter/src/lib/parsers/common/factory.js
+++ ngsi_adapter/src/lib/parsers/common/factory.js
@@ -1,19 +1,26 @@
-//
-// Copyright 2013 Telefónica I+D
-// All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License"); you may
-// not use this file except in compliance with the License. You may obtain
-// a copy of the License at
-//
-//       http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations
-// under the License.
-//
+/*
+ * Copyright 2013 Telefónica I+D
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+/**
+ * Module that implements a parser factory.
+ *
+ * @module factory
+ */
 
 
 'use strict';
@@ -24,9 +31,14 @@ var url    = require('url'),
     logger = require('../../../config/logger');
 
 
-// Parser factory: given a request URL "http://host:port/path?query", takes "path" as the name
-// of the probe whose data (request body) will be parsed. Tries to dynamically load a parser
-// object from module at lib/parsers/ directory named after the probe.
+/**
+ * Parser factory: given a request URL `http://host:port/path?query`, takes `path` as
+ * the name of the probe whose data (request body) will be parsed. Tries to dynamically
+ * load a parser object from module at `lib/parsers/` directory named after the probe.
+ *
+ * @param {http.IncomingMessage} request    The request to this server.
+ * @returns {Object} The parser been loaded according to probe mentioned in request.
+ */
 function getParser(request) {
     var probeName = url.parse(request.url).pathname.slice(1);
     try {
