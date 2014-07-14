3591491f064bea170723fdaf574092d6cc9b5b2b
Pablo Rodríguez <pablo.rodriguezarchilla@telefonica.com>
2014-07-14 09:08:07 +0200

    Add deb package generation

---
+++ ngsi_event_broker/script/build/release.sh
@@ -0,0 +1,107 @@
+#!/bin/sh
+#
+# Copyright 2013-2014 Telefónica I+D
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+
+#
+# Release a version of this component and generate .deb or .rpm package
+#
+# Usage:
+#     $0 [--help]
+#
+# Options:
+#     -h, --help	show this help message
+#
+
+OPTS='h(help)'
+NAME=$(basename $0)
+
+# Process command line
+OPTERR=
+OPTSTR=$(echo :-:$OPTS | sed 's/([a-zA-Z0-9]*)//g')
+OPTHLP=$(sed -n '20,/^$/ { s/$0/'$NAME'/; s/^#[ ]\?//p }' $0)
+while getopts $OPTSTR OPT; do while [ -z "$OPTERR" ]; do
+case $OPT in
+'h')	OPTERR="$OPTHLP";;
+'?')	OPTERR="Unknown option -$OPTARG";;
+':')	OPTERR="Missing value for option -$OPTARG";;
+'-')	OPTLONG="${OPTARG%=*}";
+	OPT=$(expr $OPTS : ".*\(.\)($OPTLONG):.*" '|' '?');
+	if [ "$OPT" = '?' ]; then
+		OPT=$(expr $OPTS : ".*\(.\)($OPTLONG).*" '|' '?')
+		OPTARG=-$OPTLONG
+	else
+		OPTARG=$(echo =$OPTARG | cut -d= -f3)
+		[ -z "$OPTARG" ] && { OPTARG=-$OPTLONG; OPT=':'; }
+	fi;
+	continue;;
+esac; break; done; done
+shift $(expr $OPTIND - 1)
+[ -z "$OPTERR" -a -n "$*" ] && OPTERR="Too many arguments"
+[ -n "$OPTERR" ] && {
+	[ "$OPTERR" != "$OPTHLP" ] && OPTERR="${OPTERR}\nTry \`$NAME --help'"
+	TAB=4; LEN=$(echo "$OPTERR" | awk -F'\t' '/\t.+\t/ {print $2}' | wc -L)
+	TABSTOPS=$TAB,$(((2+LEN/TAB)*TAB)); WIDTH=${COLUMNS:-$(tput cols)}
+	printf "$OPTERR" | tr -s '\t' | expand -t$TABSTOPS | fmt -$WIDTH -s 1>&2
+	exit 1
+}
+
+# Function to obtain GNU/Linux distro (set variable $1; OS_DISTRO if not given)
+get_linux_distro() {
+	local retvar=${1:-OS_DISTRO}
+	local distro
+	if [ -r /etc/lsb-release -a -r /etc/issue.net ]; then
+		# Ubuntu
+		distro=$(cat /etc/issue.net)
+	elif [ -r /etc/debian_version -a -r /etc/issue.net ]; then
+		# Debian
+		distro=$(cat /etc/issue.net)
+	elif [ -r /etc/redhat-release ]; then
+		# RedHat/CentOS/Fedora
+		distro=$(cat /etc/redhat-release)
+	fi
+	[ -z "$distro" ] && return 1
+	eval $retvar=\"$distro\"
+}
+
+# Function to create a .deb package
+create_deb_package() {
+	local nagios_srcdir
+	local dpkg_files dpkg_dir=$BASEDIR/..
+	cd $BASEDIR && cp -r $PROGDIR/files/debian .
+	nagios_srcdir=$(awk -F\" '/NAGIOS_SRCDIR/ {print $4}' ./config.status)
+	sed -i "s:NAGIOS_SRCDIR=:&$nagios_srcdir:" ./debian/rules
+	dpkg-buildpackage -b -rfakeroot -D -us -uc
+	dpkg_files=$(ls ../*.deb ../*.changes)
+	mv --update $dpkg_files .
+	[ -d ./debian ] && rm -rf ./debian
+}
+
+# Main
+PROGDIR=$(dirname $0)
+BASEDIR=$(readlink -f $PROGDIR/../..)
+if [ ! -r $BASEDIR/config.status ]; then
+	echo "Please run \`./configure' first" 1>&2
+	exit 2
+elif ! get_linux_distro OS_DISTRO; then
+	echo "Could not get GNU/Linux distribution" 1>&2
+	exit 3
+elif [ $(expr "$OS_DISTRO" : 'Ubuntu.*\|Debian.*') -ne 0 ]; then
+	create_deb_package
+else
+	echo "Unsupported GNU/Linux distribution" 1>&2
+	exit 4
+fi
